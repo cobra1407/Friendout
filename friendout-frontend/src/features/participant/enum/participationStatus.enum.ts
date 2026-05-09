a95a970a4e40aeafa3ce521d80a10696458dc85c
ThomasMil <thomas140797@hotmail.com>
2026-05-09 16:04:28 +0200

    refactor(participant): remove unnecessary comment and clean up code

--- friendout-frontend/src/features/participant/enum/participationStatus.enum.ts
+++ friendout-frontend/src/features/participant/enum/participationStatus.enum.ts
@@ -1,9 +1,7 @@
 export const ParticipationStatus = {
-feat/user-menu-and-admin-scaffold
     Participating: "Participating",
     Maybe: "Maybe",
     NotParticipating: "NotParticipating",
 } as const;
 
 export type ParticipationStatus = typeof ParticipationStatus[keyof typeof ParticipationStatus];
-
